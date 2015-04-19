005934bee55363e9331f142090d017f58975231a
Adam Wells <adamjwells101@gmail.com>
2015-04-19 12:54:28 -0500

    First Commit, add file output
    Added file output option to the Mario program.

--- Mario.java
+++ Mario.java
@@ -1,45 +1,54 @@
-package mario;
-
-/**
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-import java.util.*;
+import java.util.Scanner;
+import java.io.*;
 
 public class Mario {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in);
-        StringBuilder myBuilder = new StringBuilder();
-        int height;
-        do
-        { 
-           System.out.println("Enter Height of Pyramid: ");
+        StringBuilder inBuilder = new StringBuilder();
+
+        int height, outputOption;
+        do {
+            System.out.println("Enter Height of Pyramid: ");
             height = inputScanner.nextInt();
-        }while(height >23 || height < 0);
-        //start printing the pyramid
-        //start with the first row. Number Spaces is (height - 1) - (row + 2)
-        // Cont.  Number Hashes is (row + 2)
+        } while (height > 23 || height < 0);
+
+        do{
+            System.out.println("Choose Output. /n 1: Console /n 2: File");
+            outputOption = inputScanner.nextInt();
+        } while (outputOption != 1 || outputOption != 2);
         //establish the first line in Stringbuilder.  There should be Height-2 Spaces, and 2 hashes
-        for(int spaces = 0; spaces <= height-2; spaces++)
-        {
-            myBuilder.append(' ');
+
+        for (int spaces = 0; spaces <= height - 2; spaces++) {
+            inBuilder.append(' ');//start building the pyramid
+            //start with the first row. Number Spaces is (height - 1) - (row + 2)
         }
-        myBuilder.append("##");
-        //Print the first line
-        System.out.println(myBuilder.toString());
-        while(myBuilder.lastIndexOf(" ") >= 0)
-        {
-            myBuilder.setCharAt(myBuilder.lastIndexOf(" "), '#');
-            System.out.println(myBuilder.toString());
+        inBuilder.append("##");//Number Hashes is (row + 2)
+
+        if(outputOption == 1) {
+            System.out.println(inBuilder.toString());//Print the first line
+            while (inBuilder.lastIndexOf(" ") >= 0) {
+                inBuilder.setCharAt(inBuilder.lastIndexOf(" "), '#');//replace the last space with a hash.
+                System.out.println(inBuilder.toString());//print the next line.
+            }
         }
-            //replace the last space with a hash.
-            //print the next line.
-        
+        else {
+            try (PrintWriter file = new PrintWriter("pyramid.txt"))
+            {
+                file.println(inBuilder.toString());//Print the first line
+                while (inBuilder.lastIndexOf(" ") >= 0) {
+                    inBuilder.setCharAt(inBuilder.lastIndexOf(" "), '#');//replace the last space with a hash.
+                    file.println(inBuilder.toString());//print the next line.
+                }
+
+            } catch (IOException e){
+                    System.out.println("Error: " + e.toString());
+                }
+        }
+
+
     }
-    
+
 }
